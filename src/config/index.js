107872a0d3cdb2d8174f1f57ff82d32d969353ac
dharmist <dharmist@logic-square.com>
2024-10-25 23:12:35 +0530

    change the api link

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
-export const BASE_URL =
-  process.env.REACT_APP_BACKEND_ENV === "live"
-    ? ""
-    : "https://f344-223-236-99-125.ngrok-free.app/api/v1";
 // export const BASE_URL =
 //   process.env.REACT_APP_BACKEND_ENV === "live"
 //     ? ""
-//     : "http://52.66.238.171:3000/api/v1";
+//     : "https://f344-223-236-99-125.ngrok-free.app/api/v1";
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_ENV === "live"
+    ? ""
+    : "http://52.66.238.171:3000/api/v1";
 
 // process.env.REACT_APP_BACKEND_ENV === "live" ? "" : "";
 
