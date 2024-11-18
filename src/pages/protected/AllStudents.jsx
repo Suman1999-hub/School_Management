9dbf0f0fb1c5b6c57a6b20a99e3121ae8857f05d
Kavita Kumari <chauhan.kavita2248@gmail.com>
2024-11-18 17:19:09 +0530

    filter added in AllStudent

--- src/pages/protected/AllStudents.jsx
+++ src/pages/protected/AllStudents.jsx
@@ -23,12 +23,13 @@ function AllStudents() {
   const [allStudents, setAllStudents] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [availableClasses, setAvailableClasses] = useState([]);
-
+  const [selectedFilter, setSelectedFilter] = useState({});
+  console.log("selectedFilter>>", selectedFilter);
 
   const user = useSelector((state) => state.userCredential.user.loginType);
 
   useEffect(() => {
-      fetchClasses();
+    fetchClasses();
   }, []);
 
   const fetchClasses = async () => {
@@ -36,8 +37,8 @@ function AllStudents() {
 
     try {
       const response = await getAvailableClasses();
-      console.log("response>>", response.settings.availableClasses);
-      setAvailableClasses( response.settings.availableClasses)
+      // console.log("response>>", response.settings.availableClasses);
+      setAvailableClasses(response.settings.availableClasses);
     } catch (err) {
       // setError('Failed to load classes. Please try again.');
     } finally {
@@ -55,11 +56,10 @@ function AllStudents() {
       setAllStudents(studentData.students);
     } catch (err) {
       console.log("Error", err);
-    }finally {
+    } finally {
       setIsLoading(false);
     }
   };
-
   console.log("allStudents", allStudents);
 
   useEffect(() => {
@@ -71,9 +71,25 @@ function AllStudents() {
     setIsOpenModal(isOpenModal);
   };
 
+  const handleFilterChange = (event, field) => {
+    const updatedFilter = { ...selectedFilter };
+    updatedFilter[field] = event.target.value;
+    setSelectedFilter(updatedFilter);
+    ApplyFilter();
+  };
+
+  const ApplyFilter = () => {
+    return allStudents.filter((element) => {
+      const classMatch = selectedFilter.class ? element._class.name === selectedFilter.class : true
+      const sectionMatch = selectedFilter.section ? element._class.section === selectedFilter.section : true;
+      return classMatch && sectionMatch;
+    });
+  };
+  const filteredStudents = ApplyFilter();
+
   return (
     <>
-    {isLoading ? (
+      {isLoading ? (
         <div
           style={{
             textAlign: "center",
@@ -84,123 +100,134 @@ function AllStudents() {
         </div>
       ) : (
         <TabPane tabId="1">
-        {/* filter */}
-        <div className="filterWrapper">
-          <div className="filterIcon">
-            <i className="fas fa-filter" />
-          </div>
-  
-          <div className="filterForm">
-              
-            <div className="formGroup">
-              <Label>Class</Label>
-              <Input type="select">
-              <option value="">
-                   All
-                  </option>
+          {/* filter */}
+          <div className="filterWrapper">
+            <div className="filterIcon">
+              <i className="fas fa-filter" />
+            </div>
+
+            <div className="filterForm">
+              <div className="formGroup">
+                <Label>Class</Label>
+                <Input
+                  type="select"
+                  onChange={(e) => handleFilterChange(e, "class")}
+                >
+                  <option value="">All</option>
                   {availableClasses.map((classItem) => (
                     <option key={classItem.id} value={classItem.id}>
                       {classItem.grade}
                     </option>
                   ))}
-              </Input>
-            </div>
-            <div className="formGroup">
-              <Label>Section</Label>
-              <Input type="select">
-              <option value="" >
-                    All
-                  </option>
+                </Input>
+              </div>
+              <div className="formGroup">
+                <Label>Section</Label>
+                <Input
+                  type="select"
+                  onChange={(e) => handleFilterChange(e, "section")}
+                >
+                  <option value="">All</option>
                   {availableClasses.slice(0, 4).map((classItem, index) => (
                     <option key={classItem.id} value={classItem.id}>
                       {classItem.sections[index]}
                     </option>
                   ))}
-              </Input>
-            </div>
-  
-            {/* search */}
-            <div className="formGroup searchbar">
-              <Label>Search</Label>
-              <InputGroup>
-                <Input placeholder="Search..." />
-                <InputGroupText>
-                  <i className="fas fa-search" />
-                </InputGroupText>
-              </InputGroup>
-            </div>
-          </div>
-        </div>
-  
-        <div>
-          <div className="innerHeader">
-            <h2>Students</h2>
-            {user === "admin" ? (
-              <div>
-                <Button
-                  color="primary"
-                  outline
-                  className="ms-3 mx-5"
-                  onClick={() => null}
-                >
-                  Import CSV
-                </Button>
-  
-                <Button color="primary" onClick={() => _toggleModal(true)}>
-                  Add Student
-                </Button>
+                </Input>
               </div>
-            ) : (
-              ""
-            )}
+
+              {/* search */}
+              <div className="formGroup searchbar">
+                <Label>Search</Label>
+                <InputGroup>
+                  <Input placeholder="Search..." />
+                  <InputGroupText>
+                    <i className="fas fa-search" />
+                  </InputGroupText>
+                </InputGroup>
+              </div>
+              {/* <div className="formGroup">
+            <Label>Clear All Filters</Label>
+            <InputGroup>
+            <Button style={{height:"40px"}} color="primary" outline >Clear</Button>
+
+            </InputGroup>
+
+            </div> */}
+            </div>
           </div>
-  
-          <Card body>
-            <Table responsive>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Class</th>
-                  <th>Section</th>
-                  <th>Roll no</th>
-                  <th>Date of Birth</th>
-                  <th>gender</th>
-                  <th>Address</th>
-                  <th>Mobile no.</th>
-                  <th>View Details</th>
-                </tr>
-              </thead>
-  
-              <tbody>
-                {allStudents.map((curr) => {
-                  console.log(curr);
-                  return (
-                    <>
-                      <tr>
-                        <td>
-                          {curr.firstName || curr.lastName
-                            ? curr.firstName + " " + curr.lastName
-                            : ""}
-                        </td>
-  
-                        <td>{curr._class.name ? curr._class.name : ""}</td>
-                        <td>{curr._class.section ? curr._class.section : ""}</td>
-                        <td>{curr.rollNo ? curr.rollNo : ""}</td>
-                        <td>{curr.dob ? curr.dob : "" }</td>
-                        <td>{curr.gender ? curr.gender : ""}</td>
-                        <td>
-                          {curr.address
-                            ? getAddressFormate(
-                                curr.address.locality,
-                                curr.address.city,
-                                curr.address.state,
-                                curr.address.country,
-                                curr.address.pin
-                              )
-                            : ""}
-                        </td>
-                        <td>{curr.phone ? curr.phone : ""}</td>
-                        {/* <td>
+
+          <div>
+            <div className="innerHeader">
+              <h2>Students</h2>
+              {user === "admin" ? (
+                <div>
+                  <Button
+                    color="primary"
+                    outline
+                    className="ms-3 mx-5"
+                    onClick={() => null}
+                  >
+                    Import CSV
+                  </Button>
+
+                  <Button color="primary" onClick={() => _toggleModal(true)}>
+                    Add Student
+                  </Button>
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+
+            <Card body>
+              <Table responsive>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Class</th>
+                    <th>Section</th>
+                    <th>Roll no</th>
+                    <th>Date of Birth</th>
+                    <th>gender</th>
+                    <th>Address</th>
+                    <th>Mobile no.</th>
+                    <th>View Details</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {filteredStudents.map((curr) => {
+                    console.log(curr);
+                    return (
+                      <>
+                        <tr>
+                          <td>
+                            {curr.firstName || curr.lastName
+                              ? curr.firstName + " " + curr.lastName
+                              : ""}
+                          </td>
+
+                          <td>{curr._class.name ? curr._class.name : ""}</td>
+                          <td>
+                            {curr._class.section ? curr._class.section : ""}
+                          </td>
+                          <td>{curr.rollNo ? curr.rollNo : ""}</td>
+                          <td>{curr.dob ? curr.dob : ""}</td>
+                          <td>{curr.gender ? curr.gender : ""}</td>
+                          <td>
+                            {curr.address
+                              ? getAddressFormate(
+                                  curr.address.locality,
+                                  curr.address.city,
+                                  curr.address.state,
+                                  curr.address.country,
+                                  curr.address.pin
+                                )
+                              : ""}
+                          </td>
+                          <td>{curr.phone ? curr.phone : ""}</td>
+                          {/* <td>
                           <CircularProgressbar
                             value="97"
                             text="97%"
@@ -210,42 +237,43 @@ function AllStudents() {
                             })}
                           />
                         </td> */}
-                        <td>
-                          <div className="action">
-                          <Link to={`/student/${curr._id}`}>
-                            <Button color="link">
-                              <i className="fa fa-eye"></i>
-                            </Button>
-                            </Link>
-                          </div>
-                        </td>
-                      </tr>
-                    </>
-                  );
-                })}
-              </tbody>
-            </Table>
-  
-            {/* pagination */}
-            <PaginatedItems itemsPerPage={4} />
-          </Card>
-          {isOpenModal && (
-            <AddStudentModal isOpen={isOpenModal} toggle={() => _toggleModal()} />
-          )}
-          {isOpenModal && (
-          <AddStudentModal
-            isOpen={isOpenModal}
-            pageName="Add Student"
-            toggle={() => _toggleModal()}
-            fetchAllStudentData={() => fetchAllStudentData()}
-          />
-        )}
-        </div>
-      </TabPane>
+                          <td>
+                            <div className="action">
+                              <Link to={`/student/${curr._id}`}>
+                                <Button color="link">
+                                  <i className="fa fa-eye"></i>
+                                </Button>
+                              </Link>
+                            </div>
+                          </td>
+                        </tr>
+                      </>
+                    );
+                  })}
+                </tbody>
+              </Table>
 
+              {/* pagination */}
+              <PaginatedItems itemsPerPage={4} />
+            </Card>
+            {isOpenModal && (
+              <AddStudentModal
+                isOpen={isOpenModal}
+                toggle={() => _toggleModal()}
+              />
+            )}
+            {isOpenModal && (
+              <AddStudentModal
+                isOpen={isOpenModal}
+                pageName="Add Student"
+                toggle={() => _toggleModal()}
+                fetchAllStudentData={() => fetchAllStudentData()}
+              />
+            )}
+          </div>
+        </TabPane>
       )}
     </>
-   
   );
 }
 
